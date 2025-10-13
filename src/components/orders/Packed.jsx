c3a17c1340a446e63bc7d9dbeee64ee39eb76e65
Resource-04 <resource4.code4bharat@gmail.com>
2025-10-13 20:45:32 +0530

    tracking number instead of last mile number

--- src/components/orders/Packed.jsx
+++ src/components/orders/Packed.jsx
@@ -582,7 +582,10 @@ svg {
                             Packed
                           </span>
                         </td>
-                        <td className="px-4 py-3 text-sm text-gray-700">{order.lastMileAWB || "-"}</td>
+                        <td className="px-4 py-3 text-sm text-gray-700">
+  {order.shipmentDetails?.trackingNumber || "-"}
+</td>
+
                         <td className="px-4 py-3">
                           <div className="flex items-center justify-center space-x-2">
                             <button 
