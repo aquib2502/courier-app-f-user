4406b0d68a688e8e53829dd4fe55203412f42449
aquib2502 <aquibhingwala@gmail.com>
2026-02-18 16:04:21 +0530

    added google analytics header

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,6 @@
 import { GeistSans } from 'geist/font/sans';
 import { GeistMono } from 'geist/font/mono';
+import Script from 'next/script';
 import "./globals.css";
 
 export const metadata = {
@@ -10,6 +11,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`}>
+      <head>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-JE9W560T0Z"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-JE9W560T0Z');
+          `}
+        </Script>
+      </head>
       <body>{children}</body>
     </html>
   );
